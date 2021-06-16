879dfe3c07d4bfe0e0ab49e68157d03152f81fc0
dhorst <dhorst3837@student.landstede.nl>
2021-06-16 15:19:30 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Resume - Start Bootstrap Theme</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Dennis van der Horst</span>
+        <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="" alt="..."></span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Persoonlijk</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Werk ervaring</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Opleidingen</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">interesses</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Prijzen</a></li>
+        </ul>
+    </div>
+</nav>
+<!-- Page Content-->
+<div class="container-fluid p-0">
+    <!-- About-->
+    <section class="resume-section" id="about">
+        <div class="resume-section-content">
+            <h1 class="mb-0">
+                Dennis
+                <span class="text-primary">van der Horst</span>
+            </h1>
+            <div class="subheading mb-5">
+                Frans tromplaan 25, Hoevelaken ·  (31) 06 27524910 ·
+                <a href="mailto:den.horst@hotmail.com">den.horst@hotmail.com</a>
+            </div>
+            <p class="lead mb-5">Ik ben Dennis van der Horst (17), Ik ben opzoek naar een werkplek met een goede sfeer en leuke collega's. Ook ben ik ervaren in Html, Css, Php en beginnend Laravel.</p>
+            <div class="social-icons">
+                <a class="social-icon" href="https://www.linkedin.com/in/dennis-van-der-horst-641a4b204/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a class="social-icon" href="https://www.instagram.com/_dennis.horst_/" target="_blank"><svg class="svg-inline--fa fab fa-instagram fa-w-10" <i class="fab fa-instagram"></i> <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg><!-- <i class="fab fa-facebook-f"></i> Font Awesome fontawesome.com --></a>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Experience-->
+    <section class="resume-section" id="experience">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Werk Ervaring</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">McDonald's</h3>
+                    <div class="subheading mb-3">Crewlid</div>
+                    <p>Alles op de vloer van McDonald's, van keuken tot counter.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Oktober 2018 - Present</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Havi Logistics</h3>
+                    <div class="subheading mb-3">Magazijn</div>
+                    <p>In het magazijn de karren vullen die naar bepaalde restaurants en tankstations gaan.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">April 2021 - Present</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Landman Hoevelaken</h3>
+                    <div class="subheading mb-3">Bijrijder Vrachtwagen</div>
+                    <p>Meegaan op de vrachtwagen en helpen de bestellingen aan de klant te geven.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Juni 2019 - Oktober 2019</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Leaphy Robot Docent</h3>
+                    <div class="subheading mb-3">Docent</div>
+                    <p>Naar andere scholen gaan en daar uitleggen wat een leaphy is en hoe je het moet maken.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Education-->
+    <section class="resume-section" id="education">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Opleidingen</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Koningslinde</h3>
+                    <div class="subheading mb-3">Basisschool</div>
+                    <p></p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">2006 - 2014</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Corlaercollege Nijkerk</h3>
+                    <div class="subheading mb-3">Middelbare school</div>
+                    <p>Theoretische leerweg</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">2015 - 2020</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Landstede Harderwijk</h3>
+                    <div class="subheading mb-3">MBO</div>
+                    <p>ICT Software Development</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">2020 - Present</span></div>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Skills-->
+    <section class="resume-section" id="skills">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Skills</h2>
+            <div class="subheading mb-3">Code Programma's en Talen.</div>
+            <ul class="list-inline dev-icons">
+                <i class="fab fa-php"></i>
+                <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                <i class="fas fa-file-code"></i>
+                <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                <i class="fab fa-laravel"></i>
+
+            </ul>
+            <div class="subheading mb-3">Kwaliteiten</div>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    In een Team werken, maar ook Individueel.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Website mooi en afgewerkt maken.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Flexibel in het werk.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Websites responsive maken.
+                </li>
+            </ul>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Interests-->
+    <section class="resume-section" id="interests">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Interesses</h2>
+            <p>Naast het software developen hou ik ook van Gamen en wat doen met vrienden, dit kan van alles zijn, of het een lekker potje voetbal kijken is of samen naar het strand gaan.</p>
+            <p class="mb-0">Door deze periode in de corona tijd heb ik mijzelf meer gevonden als een persoon, mijn doelen vastgezet en behaald, en ik ben nu klaar voor de volgende stap in mijn carriere en het leven.</p>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Awards-->
+    <section class="resume-section" id="awards">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Prijzen</h2>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    McDonald's Medewerker van de maand
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    McDonald's Top 3 Medewerkers van het Jaar 2020.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    McDonald's Beste Keuken Crewlid 2020.
+
+                </li>
+            </ul>
+        </div>
+    </section>
+</div>
+<!-- Bootstrap core JS-->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
